f7cc1636b552ba20941a0df9f2c0bc61e482028f
Unluckytoast <161383544+Unluckytoast@users.noreply.github.com>
2024-09-21 13:04:33 -0500

    Add files via upload

---
+++ Empty/SoftSkill.java
@@ -0,0 +1,11 @@
+
+public class SoftSkill extends Skill {
+    public SoftSkill(String name, int skillLevel) {
+        super(name, "Soft", skillLevel);
+    }
+
+    @Override/* Implenting identifyskill method */
+    public void identifySkill() {
+        System.out.println("Soft Skill of " + getName() + " has a level of " + getSkillLevel());
+    }
+}
